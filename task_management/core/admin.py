74a0aa1ac924994215e2af50da0168b3c82db952
Zhanserkenov Arsen <73212575+Zhanserkenov@users.noreply.github.com>
2025-01-24 10:25:18 +0500

    second

--- task_management/core/admin.py
+++ task_management/core/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
-from .models import Task, Project, Category, Priority
+from .models import User, Task, Project
 
-admin.site.register(Task)
-admin.site.register(Project)
-admin.site.register(Category)
-admin.site.register(Priority)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'role', 'is_active')
+    list_filter = ('role', 'is_active')
+    search_fields = ('username', 'email')
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'project', 'due_date')
+    search_fields = ('title',)
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'start_date', 'end_date')
